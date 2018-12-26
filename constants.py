e775f95d9e3e5b9641bc81a487c1ab1d2a616180
Maurice Yap <mauriceyap@hotmail.co.uk>
2018-12-26 00:17:25 +0000

    correct constants variable name

--- constants.py
+++ constants.py
@@ -2,6 +2,6 @@ import os
 
 PEER_NID_SEPARATOR = ','
 PYTHON_PATH_SEPARATOR = '.'
-NID_MAPPINGS_FILEPATH = os.path.join('nid_mappings.yml')
+NID_MAPPINGS_FILE_PATH = os.path.join('nid_mappings.yml')
 USER_NODE_FILES_MODULE = 'user_node_files'
 BUFFER_SIZE = 65536
