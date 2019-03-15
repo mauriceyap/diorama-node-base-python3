7dd924e25d403f63d06433252b3db269ef3d65c5
Maurice Yap <mauriceyap@hotmail.co.uk>
2019-03-15 08:20:53 +0000

    change name nid mappings to mode addresses

--- constants.py
+++ constants.py
@@ -2,6 +2,6 @@ import os
 
 PEER_NID_SEPARATOR = ','
 PYTHON_PATH_SEPARATOR = '.'
-NID_MAPPINGS_FILE_PATH = os.path.join('nid_mappings.yml')
+NODE_ADDRESSES_FILE_PATH = os.path.join('node_addresses.yml')
 USER_NODE_FILES_MODULE = 'user_node_files'
 BUFFER_SIZE = 65536
